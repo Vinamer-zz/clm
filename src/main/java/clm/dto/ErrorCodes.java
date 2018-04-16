9331e2bdfeea959d15d27f4b2558e2ac86860368
Naman Jain <naman.jain@newgen.co.in>
2018-04-16 13:58:56 +0530

    updated code

--- src/main/java/clm/dto/ErrorCodes.java
+++ src/main/java/clm/dto/ErrorCodes.java
@@ -2,7 +2,11 @@ package clm.dto;
 
 public enum ErrorCodes {
 	SUCCESS(0001, "success"),
-	FAILURE(0002, "failure");
+	FAILURE(0002, "failure"),
+	
+	
+	//Request Validation
+	INVALID_REQUEST(4000,"Invalid request parameters");
 	
 	private final int code;
 	private final String description;
--- src/main/java/clm/service/ApplicantDetailsService.java
+++ src/main/java/clm/service/ApplicantDetailsService.java
@@ -1,8 +1,11 @@
 package clm.service;
 
+import org.springframework.stereotype.Service;
+
 import clm.dto.ApplicantDetails;
 import clm.dto.CLMResponse;
 
+@Service
 public interface ApplicantDetailsService {
 	
 	public CLMResponse saveApplicantDetails(ApplicantDetails userDetail, String arn);
