4db74b1a51069d28578dc28e6e12203fe20f9e14
Vinamer <vinamergoel1995@gmail.com>
2018-04-16 14:05:23 +0530

    Adding Entities

--- src/main/java/clm/entities/Customer.java
+++ src/main/java/clm/entities/Customer.java
@@ -1,5 +1,16 @@
 package clm.entities;
 
-public class Customer {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "CUSTOMER")
+public class Customer implements Serializable{
 
 }
