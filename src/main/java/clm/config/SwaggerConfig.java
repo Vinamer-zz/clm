207e49499908964d5afd88983564ed59575d1f91
Vinamer <vinamergoel1995@gmail.com>
2018-04-16 14:17:31 +0530

    Adding entities

--- src/main/java/clm/config/SwaggerConfig.java
+++ src/main/java/clm/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package clm.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/clm/transformer/ApplicantDetailsTransformer.java
+++ src/main/java/clm/transformer/ApplicantDetailsTransformer.java
@@ -13,29 +13,4 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApplicantDetailsTransformer {
 
-	private static final String TAX_PURPOSE_COUNTRY = "India";
-	private static final int DURATION_OF_STAY = 3;
-	private static final String OTHERS = "Others";
-
-	public void transform(@NonNull final ApplicantDetails applicantDetails,
-			@NonNull final ARNCCBridgeEntity arnBridge) {
-		log.debug("Transforming ApplicantDetails to ApplicantAdditionalInfoEntity");
-
-		// Changing the response code as user crossed the applicant details page
-		arnBridge.setResponseCode(ApplicationStatus.APPLICATION_COMPLETION_PENDING.getDescription());
-		arnBridge.setViewNumber(ApplicationExitStatus.PERSONAL_DEMOGRAPHICS.getCode());
-
-		ApplicantAdditionalInfoEntity applicantAdditionalDetails = arnBridge.getApplicant()
-				.getApplicantAdditionalInfo();
-
-		// If object already exists in database, then update the existing
-		// else create a new object
-		if (applicantAdditionalDetails == null) {
-			applicantAdditionalDetails = new ApplicantAdditionalInfoEntity();
-		}
-		
-		//save details
-
-	}
-
 }
